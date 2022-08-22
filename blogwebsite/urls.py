c244cf0801499a34fc667f2b18aaf0fd9a359e91
shiv <70441609+sk3354580@users.noreply.github.com>
2022-08-22 22:01:29 +0530

    changes

--- blogwebsite/urls.py
+++ blogwebsite/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     path('',views.blogindex,name='blogindex'),
-    path('/createblog', views.createblog, name='createblog'),
-    path('/blogregister',views.blogregister, name='blogregister'),
-    path('/bloglogin', views.bloglogin,name='bloglogin'),
-    path('/editprofile',views.editprofile,name='editprofile'),
-    path('/bloglogout', views.bloglogout,name='bloglogout'),
-    path('/blogdelete/<int:id>',views.blogdelete,name='blogdelete'),
-    path('/readblog/<int:id>',views.readblog,name='readblog'),
-    path('/blogupdate/<int:id>',views.blogupdate,name='blogupdate'),
-    path('/blogcomment/<int:id>',views.commentonblog,name = "commentonblog")
+    path('createblog', views.createblog, name='createblog'),
+    path('blogregister',views.blogregister, name='blogregister'),
+    path('bloglogin', views.bloglogin,name='bloglogin'),
+    path('editprofile',views.editprofile,name='editprofile'),
+    path('bloglogout', views.bloglogout,name='bloglogout'),
+    path('blogdelete/<int:id>',views.blogdelete,name='blogdelete'),
+    path('readblog/<int:id>',views.readblog,name='readblog'),
+    path('blogupdate/<int:id>',views.blogupdate,name='blogupdate'),
+    path('blogcomment/<int:id>',views.commentonblog,name = "commentonblog")
 ]
